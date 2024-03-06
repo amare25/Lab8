d3db95ec3a9f5d7956e206b9fc694a1dd69fbc2f
amare25 <145165639+amare25@users.noreply.github.com>
2024-03-06 16:11:43 -0700

    AddCity functionality

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+//
+// import org.junit.Before;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,16 +16,14 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
 
-    @Before
-    public void createList(){
-        list = new CustomList(null, new ArrayList<City>());
-    }
 
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>()); return list;
+    }
     @Test
     public void addCityTest(){
-        int listSize = list.getCount();
-        list.addCity(new City("Edmonton", "AB"));
-        assertEquals(list.getCount(), listSize+1);
+        list = MockCityList();
+        int listSize = list.getCount(); list.addCity(new City("Estevan", "SK")); assertEquals(list.getCount(),listSize + 1);
     }
 
 }

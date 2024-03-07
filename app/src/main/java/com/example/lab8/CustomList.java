7530327dba70d0d7ea764b05406bdf80225c3a3c
amare25 <145165639+amare25@users.noreply.github.com>
2024-03-06 22:41:57 -0700

    hasCity functionality

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -54,11 +54,11 @@ public class CustomList extends ArrayAdapter<City> {
 
     }
     public boolean hasCity(City city){
-
+        if(cities.contains(city)){
+            return true;
+        }
         return false;
     }
-
-
 
 
 
